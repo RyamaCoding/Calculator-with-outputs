b0d481432c28ce29ad11d1b8b0ca067a6c120ca4
Raghu Rayamajhi <ryamacoding@outlook.com>
2024-10-01 21:37:13 +0100

    finished calculator

---
+++ calculator.py
@@ -0,0 +1,53 @@
+import art
+import os
+
+#Calculator 
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+Operations = {"+": add, 
+              "-": subtract, 
+              "*": multiply, 
+              "/": divide}
+
+def calculator():
+    print(art.logo)
+    num1 = float(input("What's the first number?: "))
+    for symbol in Operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation = Operations[operation_symbol]
+        answer = calculation(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            os.system('cls' if os.name == 'nt' else 'clear')
+            calculator()
+
+calculator()
+
+
+
+   
+
+
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+def format_name(f_name, l_name):
+
+    f_name = input("Enter your first name: ").title()
+    l_name = input("Enter your last name: ").title()
+    print(f"Your name is {f_name} {l_name}")
+
+format_name("first_name", "last_name")
